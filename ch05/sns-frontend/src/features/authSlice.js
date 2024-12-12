e80321178d6cc952098942c6437ffaa865398814
limeunha <eh1501@naver.com>
2024-12-12 18:01:53 +0900

    up

---
+++ ch05/sns-frontend/src/features/authSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { registerUser } from '../api/snsApi'
+
+/*
+rejectWithvalue: 에러 메세지를 rejected 에 action.payload로 전달할때 사용
+rejectWithvalue 사용시 에러에 더 구체적인 내용을 담을 수 있음
+*/
+
+/*
+?(optional chaining)
+
+const error = { response: undefined }
+console.log(error.response.data.message) // TypeError 발생! (Cannot read property 'data' of undefined)
+console.log(error.response?.data?.message) // undefined 반환, 에러 없음
+*/
+
+//회원가입 thunk
+export const registerUserThunk = createAsyncThunk('auth/registerUser', async (userData, { rejectWithValue }) => {
+   try {
+      const response = await registerUser(userData)
+      return response.data.user
+   } catch (error) {
+      return rejectWithValue(error.response?.data?.message || '회원가입 실패')
+   }
+})
+
+const authSlice = createSlice({
+   name: 'auth',
+   initialState: {
+      //서버에서 가져오는 데이터가 배열 일때만 []로 초기값을 주고 나머지는 null로 준다
+      //null은 주로 문자열 혹은 json 객체 데이터 일때 사용
+      user: null,
+      loading: false,
+      error: null,
+   },
+   reducers: {},
+   extraReducers: (builder) => {
+      //회원가입
+      builder.addCase(registerUserThunk.pending, (state) => {
+         state.loading = true
+         state.error = null
+      })
+      builder.addCase(registerUserThunk.fulfilled, (state, action) => {
+         state.loading = false
+         state.user = action.payload
+      })
+      builder.addCase(registerUserThunk.rejected, (state, action) => {
+         state.loading = true
+         state.error = action.payload
+      })
+   },
+})
+
+export default authSlice.reducer
